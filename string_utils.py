b740baaaa58a86c05f60309db3c377e2c3e061d9
323103705 <rotemr2001@gmail.com>
2025-11-15 17:16:04 +0200

    Update split_before_each_uppercase function

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,25 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    upper_index = []
+    new_formula = []
+    
+    # find the index of the upper letters
+    for i in range(len(formula)):
+      if formula[i].isupper():
+        upper_index.append(i)
+
+    # slices
+    if upper_index:
+      for i in range(len(upper_index) - 1):
+        new_word = formula[upper_index[i]:upper_index[i+1]]
+        new_formula.append(new_word)
+
+      # add the last one
+      last_upper = upper_index[len(upper_index) - 1]
+      new_word = formula[last_upper:]
+      new_formula.append(new_word)
+    elif formula:
+      new_formula.append(formula)
+    return new_formula
 
 
 def split_at_first_digit(formula):

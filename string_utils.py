b873f313c775eeb7cc87af34528b5933909bc1f3
323103705 <rotemr2001@gmail.com>
2025-11-15 16:40:15 +0200

    Update split_at_first_digit function

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,11 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+  count = 0
+  for l in formula:
+    count += 1
+    if l.isdigit():
+      letters = formula[:count - 1]
+      digits = formula[count - 1:]
+      return letters, int(digits_str)
+  return formula, 1
